65912c77ecbcba9a9737641e5c79d63b01ee296f
Mickael MAS <mickael.mas@appiwedia.com>
2020-02-27 14:17:07 +0100

    Part 2 : VStack

--- Containers/ContentView.swift
+++ Containers/ContentView.swift
@@ -9,6 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var align: HorizontalAlignment = .center
+    
+    var body: some View {
+        
+        TabView {
+            ZStackView()
+                .tabItem { VStack {
+                    Image(systemName: "1.circle")
+                    Text("ZStack")
+                    }
+                }
+            
+            VStack {
+                ColorVStackView(align: $align)
+                
+                Button("Leading") {
+                    withAnimation {
+                        self.align = .leading
+                    }
+                }
+                
+                Button("Center") {
+                    withAnimation {
+                        self.align = .center
+                    }
+                }
+                
+                Button("Trailing") {
+                    withAnimation {
+                        self.align = .trailing
+                    }
+                }
+            }.tabItem { VStack {
+                    Image(systemName: "2.circle")
+                    Text("VStack")
+                    }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ZStackView: View {
     var body: some View {
         ZStack {
             Color.gray
@@ -29,8 +78,19 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ColorVStackView: View {
+    
+    @Binding var align: HorizontalAlignment
+    
+    var body: some View {
+        VStack(alignment: align, spacing: 20) {
+            RoundedRectangle(cornerRadius: 10).fill(Color.red).frame(width: 70, height: 30)
+            
+            RoundedRectangle(cornerRadius: 10).fill(Color.orange).frame(width: 200, height: 30)
+            
+            RoundedRectangle(cornerRadius: 10).fill(Color.yellow).frame(width: 100, height: 30)
+            
+        }.padding(20)
+            
     }
 }
